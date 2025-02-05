f033bcee9512b7e02d9a0d9eab3f16acf65b542d
Andrea Chello <chelloandrea@gmail.com>
2025-02-05 18:17:14 +0000

    fix: server cookie

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,5 +1,6 @@
 const isProd = process.env.NODE_ENV === "production";
 export const PORT = '4090'
 export const API_BASE_URL = isProd ?
-    `https://auth-test-bbth.vercel.app/api/v1`
+    `https://auth-test-production-7b33.up.railway.app/api/v1`
+    // `https://auth-test-bbth.vercel.app/api/v1`
     : `http://localhost:${PORT}/api/v1`
